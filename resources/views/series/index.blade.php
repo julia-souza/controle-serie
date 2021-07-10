cc1ac0d4bd42af78bd12f7bb204b4ae56338e863
Júlia <bitbucket@arcoinformatica.com.br>
2021-07-10 16:16:31 -0300

    adição botão

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -9,7 +9,7 @@
 
 @section('conteudo')
     
-    {{--  <a href="/series/criar" class="btn btn-dark mb-2"> Adicionar </a>  --}}
+    <a href="/series/criar" class="btn btn-dark mb-2"> Adicionar </a>
 
     <ul class="list-group">
         {{--  @foreach ($series as $series)
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ return view('welcome');
 });
 
 
-Route::get('/', 'SerieController@index');
+Route::get('/series', 'SerieController@index');
 Route::get('/series/criar', 'SerieController@create');
 
 Route::post('/series/criar', 'SerieController@store');
